5ac7619e5ae9bdca8cf4050788b6ffa9f1de1f9c
joshblin <146407757+joshblin@users.noreply.github.com>
2024-06-07 21:59:21 -0700

    Add files via upload

--- redis_keys.h
+++ redis_keys.h
@@ -14,3 +14,8 @@ const std::string JOINT_VELOCITIES_KEY = "sai2::cs225a::project::sensors::dq";
 const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::cs225a::project::actuators::fgc";
 const std::string CONTROLLER_RUNNING_KEY = "sai2::sim::toro::controller";
 const std::string KEYBOARD_KEY = "keyboard";
+const std::string FORCE_SENSOR_KEY_FL = "force_sensor_FL";
+const std::string FORCE_SENSOR_KEY_FR = "force_sensor_FR";
+const std::string FORCE_SENSOR_KEY_RL = "force_sensor_RL";
+const std::string FORCE_SENSOR_KEY_RR = "force_sensor_RR";
+
